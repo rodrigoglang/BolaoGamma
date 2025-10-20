225ec3ee2e76ba327879625c80d110cce51e6aac
rglang <rodrigoglang@gmail.com>
2025-10-21 00:02:13 +0200

    Added user creation

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "119480240"
+    serviceWorkerVersion: "517175064"
   }
 });
