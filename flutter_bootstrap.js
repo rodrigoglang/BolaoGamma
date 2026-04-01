c50b45953324913df600ab4181e820ecaf0430e4
rglang <rodrigoglang@gmail.com>
2026-04-01 02:04:15 +0200

    Adicionado lista de jogos do fantasy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2586385807"
+    serviceWorkerVersion: "95219810"
   }
 });
