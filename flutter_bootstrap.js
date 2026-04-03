de560713def08b2a9f624d9ea2620023f903617b
rglang <rodrigoglang@gmail.com>
2026-04-03 04:09:50 +0200

    Bandeira no nome e minor changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "670826683"
+    serviceWorkerVersion: "1915570832"
   }
 });
