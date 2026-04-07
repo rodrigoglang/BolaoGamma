61adb7bcbc8a0b748b9f3e86a263034214408cd2
rglang <rodrigoglang@gmail.com>
2026-04-07 12:24:11 +0200

    ios problem

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1278638875"
+    serviceWorkerVersion: "3014748154"
   }
 });
