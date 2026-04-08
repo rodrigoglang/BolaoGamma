34eecca767a6c890a1f514f0cf04c748db45f512
rglang <rodrigoglang@gmail.com>
2026-04-08 20:16:26 +0200

    palpites zizi

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2930823793"
+    serviceWorkerVersion: "3748070178"
   }
 });
