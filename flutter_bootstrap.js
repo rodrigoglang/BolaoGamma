a25974b043758d12f3c8c1df266f328347d54a4d
rglang <rodrigoglang@gmail.com>
2026-05-08 10:07:47 +0200

    msg zap

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1641955504" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "376117665" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
