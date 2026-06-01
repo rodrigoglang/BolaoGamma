0d3b3d1743023c99580cbd57c6c08496dd775487
rglang <rodrigoglang@gmail.com>
2026-06-01 11:09:45 +0200

    graficos

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1573472096" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "693015582" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
