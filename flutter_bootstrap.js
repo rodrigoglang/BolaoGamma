08217911e53a516fd70157600e32f3d653bf3295
rglang <rodrigoglang@gmail.com>
2026-06-13 13:22:06 +0200

    resumo diario e dia 2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "821706446" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1820078147" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
